d8049a4b7770c28cc2f7f305fb9e0e7a41dc01c4
Francisco Jimenez <francisco.jimenez@algorath.com>
2021-01-22 12:19:41 +0100

    Add FaceCapture component

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  stats: 'errors-only',
+
+  devServer: {
+    https: true,
+    contentBase: './public',
+    stats: 'minimal',
+  },
+
+  resolve: {
+    alias: {
+      // Avoid having react twice because of the local dependency
+      react: path.resolve('./node_modules/react'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // Declare babel options here, rather than in .babelrc, to make sure
+          // all the js files are resolved, including the ../.. ones
+          options: {
+            presets: [
+              // Avoid babel errors with these extra options
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              '@babel/preset-react',
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'public/index.html',
+    }),
+    new CopyPlugin([
+      {
+        from: './node_modules/@getyoti/react-face-capture/assets',
+        to: './assets',
+      },
+    ]),
+  ],
+};
