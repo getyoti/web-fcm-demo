7be91d2e408eb6753cb44be303fc68c509c5a7f2
Ivan Dyankov <ivan.dyankov@yoti.com>
2025-03-11 15:02:13 +0000

    simplify things

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import { defineConfig, normalizePath } from "vite";
 import EnvironmentPlugin from 'vite-plugin-environment'
 
-const SERVER_PORT_FALLBACK = 5000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   server: {
     proxy: {
-      "/api": `http://localhost:${process.env.SERVER_PORT || SERVER_PORT_FALLBACK}`,
+      "/api": `http://localhost:${PORT}`,
     },
   },
   plugins: [
